7981c5012bd44735dd24f2855844b1c9c44fef9c
Ramiro Jr. Franco <rjfranco@gmail.com>
2018-05-28 14:15:07 -0700

    Adding vuex to project

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import App from './App'
-import router from './router'
 import ItemTracker from '@/components/ItemTracker'
+import router from './router'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
