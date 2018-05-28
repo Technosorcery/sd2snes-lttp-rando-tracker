fcc18b9011fb5e09b700f8e75404a293614a6bb0
Ramiro Jr. Franco <rjfranco@gmail.com>
2018-05-28 16:46:33 -0700

    Comitting store options and config

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,13 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import actions from './actions'
 import App from './App'
 import ItemTracker from '@/components/ItemTracker'
+import mutations from './mutations'
 import router from './router'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  actions,
+  mutations,
+  state: {
+    game: {}
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
@@ -17,5 +27,6 @@ new Vue({
     App,
     ItemTracker
   },
+  store,
   template: '<App/>'
 })
