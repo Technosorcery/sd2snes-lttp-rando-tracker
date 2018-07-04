cf8fcd6b406de445646203827eae69885666d857
Jacob Helwig <jacob@technosorcery.net>
2018-07-03 18:13:58 -0700

    Add all dungeons to map

--- ui/src/dungeonLocations.js
+++ ui/src/dungeonLocations.js
@@ -1,5 +1,18 @@
 export default [
   {
+    name: 'Eastern Palace',
+    left: 46.8,
+    top: 38.8,
+    hoverText: 'Eastern Palace',
+    totalChests: 3,
+    dungeonCode: 'EP',
+    boss: {
+      name: 'Armos Knights',
+      hoverText: 'Armos Knights',
+      imageNumber: '02'
+    }
+  },
+  {
     name: 'Desert Palace',
     left: 3.8,
     top: 78.4,
@@ -9,7 +22,124 @@ export default [
     boss: {
       name: 'Lanmolas',
       hoverText: 'Lanmolas',
-      imageNumber: 12
+      imageNumber: '12'
+    }
+  },
+  {
+    name: 'Tower of Hera',
+    left: 31,
+    top: 5.5,
+    hoverText: 'Tower of Hera',
+    totalChests: 2,
+    dungeonCode: 'ToH',
+    boss: {
+      name: 'Moldorm',
+      hoverText: 'Moldorm',
+      imageNumber: '22'
+    }
+  },
+  {
+    name: 'Palace of Darkness',
+    left: 97,
+    top: 40,
+    hoverText: 'Palace of Darkness',
+    totalChests: 5,
+    dungeonCode: 'PoD',
+    boss: {
+      name: 'Helmasaur King',
+      hoverText: 'Helmasaur King',
+      imageNumber: '32'
+    }
+  },
+  {
+    name: 'Swamp Palace',
+    left: 73.5,
+    top: 91,
+    hoverText: 'Swamp Palace',
+    totalChests: 6,
+    dungeonCode: 'SP',
+    boss: {
+      name: 'Arrghus',
+      hoverText: 'Arrghus',
+      imageNumber: '42'
+    }
+  },
+  {
+    name: 'Skull Woods',
+    left: 53.3,
+    top: 5.4,
+    hoverText: 'Skull Woods',
+    totalChests: 2,
+    dungeonCode: 'SW',
+    boss: {
+      name: 'Mothula',
+      hoverText: 'Mothula',
+      imageNumber: '52'
+    }
+  },
+  {
+    name: "Thieves' Town",
+    left: 56.4,
+    top: 47.9,
+    hoverText: "Thieves' Town",
+    totalChests: 4,
+    dungeonCode: 'TT',
+    boss: {
+      name: 'Blind the Thief',
+      hoverText: 'Blind the Thief',
+      imageNumber: '62'
+    }
+  },
+  {
+    name: 'Ice Palace',
+    left: 89.8,
+    top: 85.8,
+    hoverText: 'Ice Palace',
+    totalChests: 3,
+    dungeonCode: 'IP',
+    boss: {
+      name: 'Kholdstare',
+      hoverText: 'Kholdstare',
+      imageNumber: '72'
+    }
+  },
+  {
+    name: 'Misery Mire',
+    left: 55.8,
+    top: 82.9,
+    hoverText: 'Misery Mire',
+    totalChests: 2,
+    dungeonCode: 'MM',
+    boss: {
+      name: 'Vitreous',
+      hoverText: 'Vitreous',
+      imageNumber: '82'
+    }
+  },
+  {
+    name: 'Turtle Rock',
+    left: 96.9,
+    top: 7,
+    hoverText: 'Turtle Rock',
+    totalChests: 5,
+    dungeonCode: 'TR',
+    boss: {
+      name: 'Trinexx',
+      hoverText: 'Trinexx',
+      imageNumber: '92'
+    }
+  },
+  {
+    name: "Ganon's Tower",
+    left: 77,
+    top: 5.5,
+    hoverText: "Ganon's Tower",
+    totalChests: 20,
+    dungeonCode: 'GT',
+    boss: {
+      name: 'Agahnim',
+      hoverText: 'Agahnim',
+      imageNumber: '102'
     }
   }
 ]
