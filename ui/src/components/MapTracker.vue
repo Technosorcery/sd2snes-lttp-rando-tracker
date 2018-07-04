52b6e502708d7611c31d6ae040c5df839ca2126c
Jacob Helwig <jacob@technosorcery.net>
2018-07-03 17:45:54 -0700

    Consolodate map logic for Dungeons & Bosses

--- ui/src/components/MapTracker.vue
+++ ui/src/components/MapTracker.vue
@@ -8,99 +8,31 @@
         :top="poi.top"
         :left="poi.left"
         :hoverText="poi.hoverText"></Location>
-      <span
+      <Dungeon
         v-for="dungeon in dungeonLocations"
         :key="dungeon.name"
-        class="mapspan dungeon"
-        :class="locationAvailability(dungeon)"
-        :style="dungeonStyle(dungeon)"></span>
-      <span
-        v-for="boss in bossLocations"
-        :key="boss.name"
-        class="mapspan boss"
-        :class="locationAvailability(boss)"
-        :style="bossStyle(boss)"></span>
+        :location="dungeon"></Dungeon>
     </div>
   </div>
 </template>
 
 <script>
+import Dungeon from '@/components/map/Dungeon.vue'
 import Location from '@/components/map/Location.vue'
 
 import poiLocations from '@/poiLocations.js'
 import dungeonLocations from '@/dungeonLocations.js'
-import bossLocations from '@/bossLocations.js'
 
 export default {
   name: 'MapTracker',
   components: {
-    Location
+    Location,
+    Dungeon
   },
   data() {
     return {
       poiLocations,
-      dungeonLocations,
-      bossLocations
-    }
-  },
-  computed: {},
-  methods: {
-    poiStyle(poi) {
-      return (
-        'background-image: url("/static/image/poi.png"); left: ' +
-        poi.left +
-        '%; top: ' +
-        poi.top +
-        '%;'
-      )
-    },
-    dungeonStyle(dungeon) {
-      return (
-        'background-image: url("/static/image/poi.png"); left: ' +
-        dungeon.left +
-        '%; top: ' +
-        dungeon.top +
-        '%;'
-      )
-    },
-    bossStyle(boss) {
-      return (
-        'background-image: url("/static/image/boss' +
-        boss.imageNumber +
-        '.png"); left: ' +
-        boss.left +
-        '%; top: ' +
-        boss.top +
-        '%;'
-      )
-    },
-    locationAvailability(location) {
-      if (
-        typeof location.available === 'undefined' ||
-        location.available.some(this.haveItemsAndDungeons)
-      ) {
-        return 'available'
-      } else if (
-        typeof location.possible !== 'undefined' &&
-        location.possible.some(this.haveItemsAndDungeons)
-      ) {
-        return 'possible'
-      }
-
-      return 'unavailable'
-    },
-    haveItemsAndDungeons(availability) {
-      return this.haveItems(availability) && this.haveDungeons(availability)
-    },
-    haveItems(availability) {
-      if (typeof availability === 'undefined') return true
-
-      return availability.items.every(item => {
-        return this.$store.state.game[item]
-      })
-    },
-    haveDungeons(availability) {
-      return true
+      dungeonLocations
     }
   }
 }
@@ -115,48 +47,5 @@ export default {
   width: 884px;
   height: 442px;
   left: 0;
-}
-
-.map .chest {
-  width: 24px;
-  height: 24px;
-  background-size: 100% 100%;
-  position: absolute;
-  margin-left: -12px;
-  margin-top: -12px;
-}
-
-.map .dungeon {
-  width: 48px;
-  height: 48px;
-  background-size: 100% 100%;
-  position: absolute;
-  margin-left: -24px;
-  margin-top: -24px;
-}
-
-.map .boss {
-  width: 24px;
-  height: 24px;
-  background: no-repeat center;
-  background-size: 75% 75%;
-  position: absolute;
-  margin-left: -12px;
-  margin-top: -12px;
-  z-index: 2;
-}
-
-.map .available {
-  background-color: rgb(0, 255, 0);
-  color: rgb(0, 0, 0);
-}
-
-.map .possible {
-  background-color: rgb(255, 255, 0);
-  color: rgb(0, 0, 0);
-}
-
-.map .unavailable {
-  background-color: rgb(255, 0, 0);
 }
 </style>
--- ui/src/dungeonLocations.js
+++ ui/src/dungeonLocations.js
@@ -1,24 +1,15 @@
 export default [
   {
     name: 'Desert Palace',
-    left: '3.8',
-    top: '78.4',
+    left: 3.8,
+    top: 78.4,
     hoverText: 'Desert Palace',
-    available: [
-      {
-        items: ['boots', 'book']
-      },
-      {
-        items: ['boots', 'flute', 'titans_mitt', 'mirror']
-      }
-    ],
-    possible: [
-      {
-        items: ['book']
-      },
-      {
-        items: ['flute', 'titans_mitt', 'mirror']
-      }
-    ]
+    totalChests: 2,
+    dungeonCode: 'DP',
+    boss: {
+      name: 'Lanmolas',
+      hoverText: 'Lanmolas',
+      imageNumber: 12
+    }
   }
 ]
